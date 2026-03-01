620067e48654d25354b6bd0e6e9a3eb89d663bf2
Shoaib Khan <shoaib7878687@gmail.com>
2026-03-01 21:48:46 +0500

    updated changes

---
+++ JavaSrcipt chap 1 to 53 Assignment/Chapter 2/script.js
@@ -0,0 +1,30 @@
+//QUESTION NO 1
+
+// var Username = "Amna Saddique"
+// console.log(Username)
+
+//QUESTION NO 2
+
+// let Myname;
+// Myname = "Amna Saddique"
+// console.log(Myname)\
+
+//QUESTION NO 3
+
+// var Messege = "Hello World"
+// alert(Messege)
+
+//QUESTION NO 4
+
+// var name = "Amna Saddique"
+// var RollNo = "19"
+// var Age = "18"
+// alert("Name is " +  name)
+// alert("Rollno is " + RollNo)
+// alert("Age is " + Age)
+
+//QUESTION NO 5
+
+var data = "plzz \n plz \n pl \n p "
+
+alert(data)
