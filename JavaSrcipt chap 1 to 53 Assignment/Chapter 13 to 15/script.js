f6c5bd194c811ee9af4848acfb68826d3ff23c3f
Shoaib Khan <shoaib7878687@gmail.com>
2026-03-07 04:57:47 +0500

    updated changes

---
+++ JavaSrcipt chap 1 to 53 Assignment/Chapter 13 to 15/script.js
@@ -0,0 +1,32 @@
+//QUESTION NO 1
+
+// let studentname = []
+// studentname.push("Amna")
+// studentname.push("Siddiqui")
+// document.write("The array name is ",studentname)
+
+//QUESTION NO 2 same as question 1
+
+//QUESTION NO 3
+
+// let name = ["Amna","Sidduiqi"] //decalre and intialize
+// document.write(name)
+
+//QUESTION NO 4
+
+// let numbers = [10,13,14]
+// document.write(numbers)
+
+//QUESTION NO 5
+
+// let boolean = ["true","false"]
+// document.write(boolean[0])
+
+//QUESTION NO 6
+
+// let mixed = ["Amna",10,true,"Sidduiqi",12,false]
+// document.write(mixed[0]) // ya jo 0 ha ya index ha yani 0,1,2,3 ya sab index ma ata ha 
+
+//QUESTION NO 7
+
+
