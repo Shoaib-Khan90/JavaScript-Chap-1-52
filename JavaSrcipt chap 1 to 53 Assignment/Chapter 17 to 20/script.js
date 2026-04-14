1e3e7d5565719fe78ab91b48fdcc0af0f36ffa29
Shoaib Khan <shoaib7878687@gmail.com>
2026-04-14 20:25:48 +0500

    updated changes

--- JavaSrcipt chap 1 to 53 Assignment/Chapter 17 to 20/script.js
+++ JavaSrcipt chap 1 to 53 Assignment/Chapter 17 to 20/script.js
@@ -108,8 +108,9 @@
 
 //QUESTION NO 10
 
-for (let i=1; i<=100 ; i++){
-    if(i%5===0){
-        document.write(i + "<br>")
-    }
-}
+// for (let i=1; i<=100 ; i++){
+//     if(i%5===0){
+//         document.write(i + "<br>")
+//     }
+// }
+
